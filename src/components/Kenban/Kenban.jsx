d8dc81ad8b0b80a7a927f4158a536ae9aedc0476
jimitas <nonajun805@gmail.com>
2023-04-01 09:22:13 +0900

    styleを当てていく

--- src/components/Kenban/Kenban.jsx
+++ src/components/Kenban/Kenban.jsx
@@ -1,6 +1,6 @@
 import { Howl } from "howler";
 import * as data from "./data";
-import styles from "src/components/Kenban/Kenban.module.css";
+import styles from "./Kenban.module.css";
 
 // var se = [""];
 // var keyDownResult;
@@ -25,11 +25,13 @@ const KENBAN = [];
 for (let i = 0; i < keyLength; i++) {
   KENBAN.push({
     id: i, //音番号
-    class: data.bgColor[i] + "Key", //黒鍵か白鍵か
+    class: data.bgColor[i], //黒鍵か白鍵か
     bgColor: data.bgColor[i], //黒鍵か白鍵か
     hz: data.hz[i], //周波数
-    toneName: data.toneName[i], //音名（CDE表記）
-    solfa: data.solfa[i], //階名 (ドレミ表記)
+    toneName1: data.toneName1[i], //音名（CDE表記）
+    toneName2: data.toneName2[i], //音名（CDE表記）♭言い換え
+    solfa1: data.solfa1[i], //階名 (ドレミ表記)
+    solfa2: data.solfa2[i], //階名 (ドレミ表記)　♭言い換え
     pushLetter1: data.pushLetter1[i], //キー情報1
     pushLetter2: data.pushLetter2[i], //キー情報2
     pushCode1: data.pushCode1[i], //キー取得コード1
@@ -43,101 +45,123 @@ export function Kenban(props) {
   return (
     <div>
       {KENBAN.map((item) => {
-        return <div key={item.id} id={item.id} className={item.class} style={{ left: `${item.leftPosition}px` }}>{item.id}</div>;
+        return (
+          <div key={item.id}>
+            <div
+              id={item.id}
+              className={
+                item.class === "white" ? `${styles.key} ${styles.whiteKey}` : `${styles.key} ${styles.blackKey}`
+              }
+              style={{ left: `${item.leftPosition}px` }}
+            >
+              <div className={styles.pushKeyName}>
+                {item.pushLetter2}
+                {<br />}
+                {item.pushLetter1}
+              </div>
+              <div className={styles.toggleButton}></div>
+              <div className={styles.keyNote}>
+                {item.toneName2}
+                {<br />}
+                {item.toneName1}
+              </div>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
-
-  // <div className="relative key_place">
-  //   <button type="text" className="relative -top-6 btn" onKeyDown={KeyDown} onKeyUp={KeyUp} value="test">
-  //     キーボード入力ON
-  //   </button>
-
-  //   <div className="absolute top-8 flex justify-center">
-  //     {W_KEY.map((W_KEY) => {
-  //       return (
-  //         <div
-  //           key={W_KEY.index}
-  //           id={W_KEY.index}
-  //           className="WH"
-  //           onMouseDown={Play_WH}
-  //           onTouchStart={Play_WH}
-  //           onMouseUp={Pause_WH}
-  //           onTouchEnd={Pause_WH}
-  //         >
-  //           {W_KEY.note}
-  //           <br />
-  //           {W_KEY.label}
-  //         </div>
-  //       );
-  //     })}
-
-  //   var gakki = "ke-";
-
-  //   switch (props.gakki) {
-  //     case "けんばんハーモニカ":
-  //       gakki = "ke-";
-  //       break;
-  //     case "リコーダー":
-  //       gakki = "re_";
-  //       break;
-  //     case "もっきん":
-  //       gakki = "mo_";
-  //       break;
-  //     case "てっきん":
-  //       gakki = "te_";
-  //       break;
-  //   }
-
-  //   //何のキーが押されたかを判定してコードを返す
-  //   const check_code = (e) => {
-  //     return (keyDownResult = KEY_CODE.indexOf(e.code));
-  //   };
-
-  //   const Play_BK = (e) => {
-  //     const index = ITEMS_BK_INDEX.indexOf(Number(e.target.id));
-  //     se[ITEMS_BK_INDEX[index]].play();
-  //   };
-
-  //   const Pause_BK = (e) => {
-  //     const index = ITEMS_BK_INDEX.indexOf(Number(e.target.id));
-  //     se[ITEMS_BK_INDEX[index]].pause();
-  //     se[ITEMS_BK_INDEX[index]].seek(0);
-  //   };
-
-  //   const Play_WH = (e) => {
-  //     const index = ITEMS_WH_INDEX.indexOf(Number(e.target.id));
-  //     se[ITEMS_WH_INDEX[index]].play();
-  //   };
-
-  //   const Pause_WH = (e) => {
-  //     const index = ITEMS_WH_INDEX.indexOf(Number(e.target.id));
-  //     se[ITEMS_WH_INDEX[index]].pause();
-  //     se[ITEMS_WH_INDEX[index]].seek(0);
-  //   };
-
-  //   const KeyDown = (e) => {
-  //     check_code(e);
-  //     if (Key_flag[keyDownResult] === false) {
-  //       Key_flag[keyDownResult] = true;
-  //       se[keyDownResult].play();
-  //       document.getElementById(keyDownResult).style.backgroundColor = "rgba(252, 165, 165)";
-  //     }
-  //   };
-
-  //   const KeyUp = (e) => {
-  //     check_code(e);
-  //     if (Key_flag[keyDownResult] === true) {
-  //       Key_flag[keyDownResult] = false;
-  //       se[keyDownResult].pause();
-  //       se[keyDownResult].seek(0);
-  //       //直接DOMを触らずにできる方法があれば尚いいのだが。
-  //       // エレメントrefを配列化できるといいのかも…
-  //       if (document.getElementById(keyDownResult).className == "WH") {
-  //         document.getElementById(keyDownResult).style.backgroundColor = "";
-  //       } else {
-  //         document.getElementById(keyDownResult).style.backgroundColor = "";
-  //       }
-  //     }
-  // };
 }
+
+// <div className="relative key_place">
+//   <button type="text" className="relative -top-6 btn" onKeyDown={KeyDown} onKeyUp={KeyUp} value="test">
+//     キーボード入力ON
+//   </button>
+
+//   <div className="absolute top-8 flex justify-center">
+//     {W_KEY.map((W_KEY) => {
+//       return (
+//         <div
+//           key={W_KEY.index}
+//           id={W_KEY.index}
+//           className="WH"
+//           onMouseDown={Play_WH}
+//           onTouchStart={Play_WH}
+//           onMouseUp={Pause_WH}
+//           onTouchEnd={Pause_WH}
+//         >
+//           {W_KEY.note}
+//           <br />
+//           {W_KEY.label}
+//         </div>
+//       );
+//     })}
+
+//   var gakki = "ke-";
+
+//   switch (props.gakki) {
+//     case "けんばんハーモニカ":
+//       gakki = "ke-";
+//       break;
+//     case "リコーダー":
+//       gakki = "re_";
+//       break;
+//     case "もっきん":
+//       gakki = "mo_";
+//       break;
+//     case "てっきん":
+//       gakki = "te_";
+//       break;
+//   }
+
+//   //何のキーが押されたかを判定してコードを返す
+//   const check_code = (e) => {
+//     return (keyDownResult = KEY_CODE.indexOf(e.code));
+//   };
+
+//   const Play_BK = (e) => {
+//     const index = ITEMS_BK_INDEX.indexOf(Number(e.target.id));
+//     se[ITEMS_BK_INDEX[index]].play();
+//   };
+
+//   const Pause_BK = (e) => {
+//     const index = ITEMS_BK_INDEX.indexOf(Number(e.target.id));
+//     se[ITEMS_BK_INDEX[index]].pause();
+//     se[ITEMS_BK_INDEX[index]].seek(0);
+//   };
+
+//   const Play_WH = (e) => {
+//     const index = ITEMS_WH_INDEX.indexOf(Number(e.target.id));
+//     se[ITEMS_WH_INDEX[index]].play();
+//   };
+
+//   const Pause_WH = (e) => {
+//     const index = ITEMS_WH_INDEX.indexOf(Number(e.target.id));
+//     se[ITEMS_WH_INDEX[index]].pause();
+//     se[ITEMS_WH_INDEX[index]].seek(0);
+//   };
+
+//   const KeyDown = (e) => {
+//     check_code(e);
+//     if (Key_flag[keyDownResult] === false) {
+//       Key_flag[keyDownResult] = true;
+//       se[keyDownResult].play();
+//       document.getElementById(keyDownResult).style.backgroundColor = "rgba(252, 165, 165)";
+//     }
+//   };
+
+//   const KeyUp = (e) => {
+//     check_code(e);
+//     if (Key_flag[keyDownResult] === true) {
+//       Key_flag[keyDownResult] = false;
+//       se[keyDownResult].pause();
+//       se[keyDownResult].seek(0);
+//       //直接DOMを触らずにできる方法があれば尚いいのだが。
+//       // エレメントrefを配列化できるといいのかも…
+//       if (document.getElementById(keyDownResult).className == "WH") {
+//         document.getElementById(keyDownResult).style.backgroundColor = "";
+//       } else {
+//         document.getElementById(keyDownResult).style.backgroundColor = "";
+//       }
+//     }
+// };
